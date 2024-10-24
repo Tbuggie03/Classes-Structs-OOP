c700f223074acd888c8fb82096ce0cd1fb69ebaf
Tommy Seternus <thomassseternus@gmail.com>
2024-10-24 08:24:37 -0400

    added character class and new hero

--- Participation Classes/Participation Classes/Program.cs
+++ Participation Classes/Participation Classes/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Participation_Classes
 {
+    public class Character 
+    {
+        public string name;
+        public int exp = 0;
+    
+    }
+
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Character hero = new Character();
+            Console.WriteLine("Hero: " + hero.name + " - " + hero.exp + " EXP");
+            
         }
     }
 }
