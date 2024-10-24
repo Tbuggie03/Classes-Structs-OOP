dbab1e8ca598e130caf8fd505509df7089de6d38
Tommy Seternus <thomassseternus@gmail.com>
2024-10-24 09:06:40 -0400

    created dude and finished

--- Participation Classes/Participation Classes/Program.cs
+++ Participation Classes/Participation Classes/Program.cs
@@ -109,6 +109,9 @@
             Adventurer mike = new Adventurer("Mike");
             mike.PrintStatsInfo();
 
+            Dude dave = new Dude("Dave");
+            dave.PrintStatsInfo();
+
         }
     }
 }
